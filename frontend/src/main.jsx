af2fe26a6787988af571d884f0599a564a17a75f
Kumar Anshuman <kanshuman6484@gmail.com>
2025-04-20 00:22:54 +0530

    fix main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import UserProvider from './contexts/UserContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )
